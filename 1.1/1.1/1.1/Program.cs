ec287c2b70900f120582846e1f74b6a52e1e7b52
Galib Rahman <mdgalib@hotmail.com>
2017-04-17 15:57:48 -0500

    program.cs changes

--- 1.1/1.1/1.1/Program.cs
+++ 1.1/1.1/1.1/Program.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace _1._1
 {
     class Program
     {
+        public static void ThreadMethod() {
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThredProc {0}", i);
+                Thread.Sleep(0);
+            }
+        }
+
         static void Main(string[] args)
         {
+            Thread t = new Thread(new ThreadStart(ThreadMethod));
         }
     }
 }
